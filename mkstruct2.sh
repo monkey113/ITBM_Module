adccb1a233ed66dab43379c6e5283c6ba1af823a
monkey113 <monkey113@gmx.ch>
2021-08-11 21:30:55 +0000

    Added mkstruct2

---
+++ mkstruct2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+exec 2>/dev/null
+
+KURSNAME=${KURSNAME:-"Kurs"}
+
+if [[ ! -z $1 ]]; then
+	KURSNAME=$1
+fi
+
+rootfolder=~/mkstruct/$KURSNAME
+
+if mkdir $rootfolder; then
+	echo "mkdir -p $rootfolder"
+	echo "mkdir $rootfolder/block1"
+	echo "cd $rootfolder/block1"
+	exit 0
+fi
+
+echo "Ordner kann nicht angelegt werden"
+exit 1
+
