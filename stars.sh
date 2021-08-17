414dcef4d986636b0c1b49aaae69bd6854175922
monkey113 <monkey113@gmx.ch>
2021-08-17 12:59:34 +0000

    Final script, working thanks to the sample solutions as I couldn't figure out a shell-expansion problem with the star character (gets expanded as wildcard in the shell and just outputs the files of the currend wd)

--- stars.sh
+++ stars.sh
@@ -7,10 +7,14 @@
 # ================================== 
 
 amntStars=$1
+stars=
 
 if [ -z $1 ] || ! [ $1 -eq $1 ] 2>/dev/null; then
 	echo "Usage: $(basename $0) [desired amount of stars (integer)]"
 	exit
 fi
 
-
+for (($amntStars; amntStars >= 1; amntStars--)); do
+	# stars+="*"   #non-working attempt, as the wildcard gets expanded by the shell itself, while executing. Neither Escape characters nor escaping can fix that.
+	echo -n \* # got this line from the sample solutions as I couldn't find a work-around for the shell-expansion problem
+done
