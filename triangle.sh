39b02b59e7523271a4ccbf48d067948fb52cfcb6
monkey113 <monkey113@gmx.ch>
2021-08-18 10:19:53 +0000

    Comments added

--- triangle.sh
+++ triangle.sh
@@ -13,9 +13,9 @@ if [ -z $1 ] || ! [ $1 -eq $1 ] 2>/dev/null; then
 	exit
 fi
 
-for ((counter=1; counter <= amntStars; counter++)); do
-	for ((counter2=1; counter2 <= counter; counter2++)); do
-	echo -n \*
+for ((counter=1; counter <= amntStars; counter++)); do		# seting a counter. Loop continues as long as counter is not equal to the desired amount if stars. Counter gets increased after every iteration. This is the same as in the stars.sh file.
+	for ((counter2=1; counter2 <= counter; counter2++)); do # nested for-Loop beginns here. A second counter is defined. The loops condition is met as long as counter2 doesn't equal the counter of the main-loop. Thus it runs every iteration once more than in the last until the counter of the main-loop reaches the desired amount of stars). The counter of this sub-loop gets upped after every itearation to finally match the counter of the main-loop.
+		echo -n \* # got this from the official solutions as I had problems with shell expansion. I tried an attempt where the stars get written in variable (appending) using variable+="*". But this always gets expanded by the shell during execution, so no valid attempt for this exact case (using wildcards).
 	done
-	echo ""
+	echo "" # way to make a new line after every iteration, otherwise the stars will be written on the same line due to the behavior of echo -n
 done
